928f9c7b0484627f99c9961bc6e7d146c323676d
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-12-18 12:22:49 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,13 @@ import morgan from 'morgan';
 import { connectDB } from './config/db';
 import { errorHandler } from './middleware/error';
 import routes from './routes';
-import App from '@/app';
-import 'module-alias/register';
-import 'reflect-metadata';
 
 // Load environment variables
 config();
 
 // Initialize express
 const app = express();
-const httpServer = http.createServer(app);
+const server = http.createServer(app);
 
 // Connect to Database
 connectDB();
@@ -43,19 +40,15 @@ app.use(errorHandler);
 const port = parseInt(process.env.PORT || '5001', 10);
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-// Create an instance of the App class
-const appInstance = new App(port);
-
-// Start the server
-httpServer.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running in ${NODE_ENV} mode on port ${port}`);
 });
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err: any) => {
   console.error(`Error: ${err.message}`);
-  // Close the HTTP server and exit the process
-  httpServer.close(() => process.exit(1));
+  // Close the server and exit the process
+  server.close(() => process.exit(1));
 });
 
-export { app, httpServer };
+export { app, server };
